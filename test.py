5e896a4dbb657fea3f790290f59f0cc79b667fee
Michał Przyłucki <michalprzyl@gmail.com>
2024-08-24 00:21:59 +0200

    Add tabs

--- test.py
+++ test.py
@@ -4,17 +4,14 @@ def main():
     os.makedirs('main_app/migrations')
     create_migration_for_application("main_app", '0001', 'start', [])
     create_migration_for_application("main_app", '0002', 'another_one', [('main_app', '0001_start')])
-    create_migration_for_application("main_app", '0003', 'another_one_third', [('main_app', '0002_another_one')])
+    create_migration_for_application("main_app", '0003', 'another_one_third', [('main_app', '0002_another_one'), ('main_app', '0069_hello_darkness_my_old_friend')])
 
 def create_migration_for_application(
         app: str, 
         migration_number: int,
         migration_file_name: str,
         dependencies: list[str]):
-    dependencies_str = ", ".join([f'("{dep[0]}", "{dep[1]}")' for dep in dependencies])
-    print(f"dependencies_str: {dependencies_str}")
-            # """from django.db import migrations, models
-
+    dependencies_str = ",\n\t\t\t\t\t\t".join([f'("{dep[0]}", "{dep[1]}")' for dep in dependencies])
 
     migration_content = f"""class Migration(migrations.Migration):
         dependencies = [{dependencies_str}]
